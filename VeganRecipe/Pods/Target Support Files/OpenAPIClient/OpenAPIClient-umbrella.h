d98bbb652a066cdeb525aeafa51d33ef48a7ce9d
Slava <tmp@mail.com>
2019-10-02 10:11:15 +0300

    Add SDK in project

---
+++ VeganRecipe/Pods/Target Support Files/OpenAPIClient/OpenAPIClient-umbrella.h
@@ -0,0 +1,39 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "OAIDefaultApi.h"
+#import "JSONValueTransformer+ISO8601.h"
+#import "OAIApi.h"
+#import "OAIApiClient.h"
+#import "OAIBasicAuthTokenProvider.h"
+#import "OAIConfiguration.h"
+#import "OAIDefaultConfiguration.h"
+#import "OAIJSONRequestSerializer.h"
+#import "OAILogger.h"
+#import "OAIObject.h"
+#import "OAIQueryParamCollection.h"
+#import "OAIResponseDeserializer.h"
+#import "OAISanitizer.h"
+#import "OAIInlineObject.h"
+#import "OAIInlineObject1.h"
+#import "OAIInlineObject2.h"
+#import "OAIInlineObject3.h"
+#import "OAIInlineObject4.h"
+#import "OAIInlineObject5.h"
+#import "OAIInlineObject6.h"
+#import "OAIInlineObject7.h"
+#import "OAIInlineObject8.h"
+#import "OAIInlineObject9.h"
+
+FOUNDATION_EXPORT double OpenAPIClientVersionNumber;
+FOUNDATION_EXPORT const unsigned char OpenAPIClientVersionString[];
+
---
+++ VeganRecipe/VeganRecipe/Api-Bridge.h
@@ -0,0 +1,30 @@
+//
+//  Api-Bridge.h
+//  VeganRecipe
+//
+//  Created by Slava on 9/22/19.
+//  Copyright © 2019 Slava. All rights reserved.
+//
+
+#ifndef Api_Bridge_h
+#define Api_Bridge_h
+
+#import <OpenAPIClient/OAIApiClient.h>
+#import <OpenAPIClient/OAIDefaultConfiguration.h>
+// load models
+#import <OpenAPIClient/OAIInlineObject.h>
+#import <OpenAPIClient/OAIInlineObject1.h>
+#import <OpenAPIClient/OAIInlineObject2.h>
+#import <OpenAPIClient/OAIInlineObject3.h>
+#import <OpenAPIClient/OAIInlineObject4.h>
+#import <OpenAPIClient/OAIInlineObject5.h>
+#import <OpenAPIClient/OAIInlineObject6.h>
+#import <OpenAPIClient/OAIInlineObject7.h>
+#import <OpenAPIClient/OAIInlineObject8.h>
+#import <OpenAPIClient/OAIInlineObject9.h>
+// load API classes for accessing endpoints
+#import <OpenAPIClient/OAIDefaultApi.h>
+
+
+
+#endif /* Api_Bridge_h */
